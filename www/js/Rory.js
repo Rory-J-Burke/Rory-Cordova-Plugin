2882e6e36a903130ae29387eb4629bdde5792c41
rory <thundersnow716@gmail.com>
2021-01-05 15:15:09 -0500

    added import/export

--- www/js/Rory.js
+++ www/js/Rory.js
@@ -2,7 +2,7 @@
 provide callback to handle styling or other front end processing of the result string
 */
 
-rory = function(input, callback) {
+export default rory = function(input, callback) {
     cordova.exec(
         function(callback, output) {
             callback(output);
